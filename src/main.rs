615906fa2e74a406a9bfca1af164c355c3adcc2a
Kyle Forrester <kforrester@manh.com>
2020-11-25 17:47:20 -0500

    Adding multithreading support for video generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::io::{self, Write};
+use std::sync::mpsc;
+use std::thread;
 
 mod mandelbrot;
 
@@ -13,24 +15,64 @@ enum ArgState {
     Algorithm,
     Zoom,
     FrameCount,
+    ThreadCount,
 }
 
+#[derive(Copy, Clone)]
 enum Algorithm {
     Mandelbrot,
 }
 
 fn main() {
-    let (focus_x, focus_y, mut range, width, height, algorithm, zoom, frames) = parse_args();
+    let (focus_x, focus_y, mut range, width, height, algorithm, zoom, frames, threads) = parse_args();
 
-    // Loop through all the frames
-    for _i in 0..frames {
-        let header = format!("P6\n{} {}\n255\n", width, height);
-        write_image(header, focus_x, focus_y, range, width, height, &algorithm);
+    let full_multiprocess_loops = frames/threads;
+    let remainder = frames - (full_multiprocess_loops * threads);
+
+    for _i in 0..full_multiprocess_loops {
+        let mut channels = Vec::new();
+        for _y in 0..threads {
+            let (tx, rx) = mpsc::channel();
+            channels.push(rx);
+
+            let new_alg = algorithm.clone();
+            thread::spawn(move || {
+                let buffer = gen_image(focus_x, focus_y, range, width, height, new_alg);
+                tx.send(buffer).unwrap();
+            });
+            range *= zoom;
+        }
+
+        // Gather images generated from other threads
+        for rx in channels {
+            let buffer = rx.recv().unwrap();
+            io::stdout().write_all(&buffer[..]).expect("Error: Failure to write to stdout!");
+        }
+    }
+
+    // Finish with less than full thread count
+    let mut channels = Vec::new();
+    for _y in 0..remainder{
+        let (tx, rx) = mpsc::channel();
+        channels.push(rx);
+
+        let new_alg = algorithm.clone();
+        thread::spawn(move || {
+            let buffer = gen_image(focus_x, focus_y, range, width, height, new_alg);
+            tx.send(buffer).unwrap();
+        });
         range *= zoom;
     }
+
+    // Gather images generated from other threads
+    for rx in channels {
+        let buffer = rx.recv().unwrap();
+        io::stdout().write_all(&buffer[..]).expect("Error: Failure to write to stdout!");
+    }
+
 }
 
-fn parse_args() -> (f64, f64, f64, u32, u32, Algorithm, f64, u32) {
+fn parse_args() -> (f64, f64, f64, u32, u32, Algorithm, f64, u32, u32) {
     let mut state = ArgState::Initial;
     let mut focus_x = -1f64;
     let mut focus_y = 0f64;
@@ -40,6 +82,7 @@ fn parse_args() -> (f64, f64, f64, u32, u32, Algorithm, f64, u32) {
     let mut algorithm = Algorithm::Mandelbrot;
     let mut zoom = 0.99;
     let mut frames = 1;
+    let mut threads = 1;
     for arg in env::args().skip(1) {
         match arg.as_str() {
             "-x" | "--x_coor" => state = ArgState::FocalX,
@@ -50,6 +93,7 @@ fn parse_args() -> (f64, f64, f64, u32, u32, Algorithm, f64, u32) {
             "-a" | "--algorithm" => state = ArgState::Algorithm,
             "-z" | "--zoom" => state = ArgState::Zoom,
             "-f" | "--frames" => state = ArgState::FrameCount,
+            "-t" | "--threads" => state = ArgState::ThreadCount,
             "-h" | "--help" => help_args(),
             _ => match state {
                 ArgState::FocalX => focus_x = arg.parse().expect(format!("{} must be a floating point value!", arg).as_str()),
@@ -68,6 +112,7 @@ fn parse_args() -> (f64, f64, f64, u32, u32, Algorithm, f64, u32) {
                 },
                 ArgState::Zoom => zoom = arg.parse().expect(format!("{} must be a floating point value!", arg).as_str()),
                 ArgState::FrameCount => frames = arg.parse().expect(format!("{} must be a positive integral value!", arg).as_str()),
+                ArgState::ThreadCount => threads = arg.parse().expect(format!("{} must be a positive integral value!", arg).as_str()),
                 ArgState::Initial => {
                     eprintln!("Error unknown argument {}! Expected either flags -x, -y, -r, -w, -h, -a, -z, -f, or -h", arg);
                     help_args();
@@ -76,7 +121,7 @@ fn parse_args() -> (f64, f64, f64, u32, u32, Algorithm, f64, u32) {
         }
     }
 
-    (focus_x, focus_y, range, width, height, algorithm, zoom, frames)
+    (focus_x, focus_y, range, width, height, algorithm, zoom, frames, threads)
 }
 
 fn help_args() {
@@ -102,17 +147,20 @@ fn help_args() {
     println!("-f | --frames         : Number of images to take, zooming in to the focal point for each image");
     println!("                        Set -f to 1 for a single image, or >= 2 for a video");
     println!("                        Default: 1");
+    println!("-t | --threads        : Number of threads to multiprocess the image");
+    println!("                        Default: 1");
     println!("-h | --help           : See help output.  Trumps other argument flags");
     println!("\nHere is an example execution of mandelart to generate a single image of mandelbrot set:");
     println!("mandelart -x -0.754 -y 0.05 -r 0.001 -w 1920 -ht 1080 -a mandelbrot -f 1");
-    println!("Here is an example execution of mandelart to generate a 60 second video of mandelbrot set at 60 fps:");
-    println!("mandelart -x -0.754 -y 0.05 -r 0.001 -w 1920 -ht 1080 -a mandelbrot -z 0.99 -f 3600");
+    println!("Here is an example execution of mandelart to generate a 60 second video of mandelbrot set at 60 fps using 6 threads:");
+    println!("mandelart -x -0.754 -y 0.05 -r 0.001 -w 1920 -ht 1080 -a mandelbrot -z 0.99 -f 3600 -t 6");
 
     std::process::exit(0);
 }
 
-fn write_image(header: String, focus_x: f64, focus_y: f64, range: f64, width: u32, height: u32, algorithm: &Algorithm) {
+fn gen_image(focus_x: f64, focus_y: f64, range: f64, width: u32, height: u32, algorithm: Algorithm) -> Vec::<u8> {
     let mut buffer = Vec::<u8>::new();
+    let header = format!("P6\n{} {}\n255\n", width, height);
     buffer.extend_from_slice(header.as_bytes());
     let step_size = range / f64::from(width);
     let start_x = focus_x - (range / 2f64);
@@ -135,5 +183,5 @@ fn write_image(header: String, focus_x: f64, focus_y: f64, range: f64, width: u3
     }
     buffer.push(b"\n"[0]);
 
-    io::stdout().write_all(&buffer[..]).expect("Error: Failure to write to stdout!");
+    buffer
 }
